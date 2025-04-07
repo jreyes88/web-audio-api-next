104b0a64d792622d4431b4df701b4b5851b48598
jjooeeyy <jreyes88@gmail.com>
2025-04-07 08:59:47 -0500

    chore: styling sliders

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className={styles.app}>
       <header className="header">
-        <h1>Web Audio API in React</h1>
+        <h1 className={styles.heading}>Web Audio API in React</h1>
       </header>
       <div className={styles.synth}>
         <LFO />
