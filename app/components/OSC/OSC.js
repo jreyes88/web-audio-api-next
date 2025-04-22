8433972704cac873140c0ba749c681b34d5a8973
jjooeeyy <jreyes88@gmail.com>
2025-04-22 15:59:58 -0500

    chore: simplify, start with OSC

--- app/components/OSC/OSC.js
+++ app/components/OSC/OSC.js
@@ -38,21 +38,8 @@ export default function OSC() {
       <div className="param">
         <label htmlFor="type">Wave (Type)</label>
         <select id="type" onChange={changeType} value={type}>
-          <option value="sine">
-            <span className={styles.hide}>Sine </span>∿
-          </option>
-          <option value="square">
-            <span>Square </span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#000000"
-              width="800px"
-              height="800px"
-              viewBox="0 -64 640 640"
-            >
-              <path d="M476 480H324a36 36 0 0 1-36-36V96h-96v156a36 36 0 0 1-36 36H16a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h112V68a36 36 0 0 1 36-36h152a36 36 0 0 1 36 36v348h96V260a36 36 0 0 1 36-36h140a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H512v156a36 36 0 0 1-36 36z" />
-            </svg>
-          </option>
+          <option value="sine">Sine</option>
+          <option value="square">Square</option>
           <option value="sawtooth">Sawtooth</option>
           <option value="triangle">Triangle</option>
         </select>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,6 @@
 "use client";
-// import Osc1 from "./components/Osc1";
-import Filter from "./components/Filter";
+import OSC from "./components/OSC/OSC";
 import Keyboard from "./components/Keyboard";
-import ADSR from "./components/ADSR";
-
-import LFO from "./components/LFO/LFO";
-import Osc from "./components/OSC/OSC";
-
 import styles from "./page.module.scss";
 
 export default function Home() {
@@ -16,11 +10,7 @@ export default function Home() {
         <h1 className={styles.heading}>Web Audio API in React</h1>
       </header>
       <div className={styles.synth}>
-        <LFO />
-        <Osc />
-        {/* <Osc1 /> */}
-        <ADSR />
-        <Filter />
+        <OSC />
         <Keyboard />
       </div>
     </div>
